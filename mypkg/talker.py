e8a069a093c6121acb8b0dcb9c3a110fc6d8757b
Kaito Suzuki <kaito160306@gmail.com>
2023-11-30 13:34:13 +0900

    Change a command

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,13 +3,10 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 class Talker():
-    def __init__(self):
+    def __init__(self., node):
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
-
-rclpy.init()
-node = Node("talker")
-talker = Talker()
+        node.create_timer(0.5, cb)
 
 def cb():
     global n
@@ -18,5 +15,7 @@ def cb():
     talker.pub.publish(msg)
     talker.n += 1
 
-node.create_timer(0.5, cb)
+rclpy.init()
+node = Node("talker")
+talker = Talker()
 rclpy.spin(node)
