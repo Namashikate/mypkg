baed84297fa0f2310436a6fdee8c81d6b93fe0a4
Kaito Suzuki <kaito160306@gmail.com>
2023-11-30 13:49:04 +0900

    Change a command

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -8,12 +8,11 @@ class Talker():
         self.n = 0
         node.create_timer(0.5, cb)
 
-def cb():
-    global n
-    msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    talker.n += 1
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
 
 rclpy.init()
 node = Node("talker")
